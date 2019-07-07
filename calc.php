064e1af44eeea3e92b4257d7149df4438c2b2f4e
VikaHavrylyuk <vikahavrylyuk@gmail.com>
2019-07-07 20:30:59 +0300

    Додала функції додавання, віднімання, множення та ділення

--- calc.php
+++ calc.php
@@ -1,12 +1,44 @@
 <center>
 	<h1>Калькулятор</h1>
-<form action="?calc" method="POST">
+<form method="POST">
     <input type="text" name="x1">
     <select name="operation">
         <option value="+">+</option>
         <option value="-">-</option>
+		<option value="*">*</option>
+		<option value="/">/</option>
     </select>
     <input type="text" name="x2">
-    <input type="submit" value="Посчитать">
+    <input type="submit" value="Посчитать" name="sub">
 </form>
 </center>
+
+
+<? 
+
+if (isset($_POST['sub'])) {
+	if ($_POST['operation'] == "+"){
+		$res = $_POST['x1'] + $_POST['x2'];
+	}
+	
+	if ($_POST['operation'] == "-"){
+		$res = $_POST['x1'] - $_POST['x2'];
+	}
+	
+	if ($_POST['operation'] == "*"){
+		$res = $_POST['x1'] * $_POST['x2'];
+	}
+	
+	if ($_POST['operation'] == "/"){
+		$res = $_POST['x1'] / $_POST['x2'];
+	}
+	
+	echo '<center>';
+	echo '<br>';
+	echo "Результат обрахунку:";
+	echo '<br>';
+	$expression = $_POST['x1'] . ' ' . $_POST['operation'] . ' ' . $_POST['x2'] . ' = ';
+	echo $expression . $res;
+	echo '</center>';
+}
+?>
